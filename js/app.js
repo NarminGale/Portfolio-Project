feb4bba987f2bbbe61a397d8d32f78005cba04e3
NarminGale <narmingale@gmail.com>
2021-03-12 15:52:53 +0400

    Completed navbar fixed functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ const navBtn = document.getElementById('nav-btn')
 const closeBtn = document.getElementById('close-btn')
 const sidebar = document.getElementById('sidebar')
 
+const navbar = document.getElementById('nav')
+
+// add fixed class to navbar
+window.addEventListener('scroll', function () {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add('navbar-fixed')
+  } else {
+    navbar.classList.remove('navbar-fixed')
+  }
+})
+
+// show sidebar
 navBtn.addEventListener('click', function () {
   sidebar.classList.add('show-sidebar')
 })
